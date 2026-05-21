e3ef8dd8d4af6b8e631d00cba5e4ccd49be7a231
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2026-05-20 22:26:47 -0300

    Gustavo Henrique, Mauro Roque, Felipe Sardinha

--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/AppRpgEtec.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/AppRpgEtec.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppRpgEtec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4a177097b599cb3f65157d15e2cf0eb8fa5f8dc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d3601f2feeef4b9bc50efc260ebefc9b7585e8e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppRpgEtec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppRpgEtec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
