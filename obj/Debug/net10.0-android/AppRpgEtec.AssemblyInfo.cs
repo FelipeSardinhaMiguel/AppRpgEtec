2e3802a98593162f221431e807aa04807db659d4
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2026-05-27 22:26:59 -0300

    Aula 14 parte um - completa

--- obj/Debug/net10.0-android/AppRpgEtec.AssemblyInfo.cs
+++ obj/Debug/net10.0-android/AppRpgEtec.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppRpgEtec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e3ef8dd8d4af6b8e631d00cba5e4ccd49be7a231")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0f1028bd8438927bf3cffe06ba301fa6b4b17051")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppRpgEtec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppRpgEtec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
